4db30484efe0d9d86442bd76c5f874d83ce0e2e7
bahaqurini <bahaa.qurini@gmail.com>
2025-10-18 21:18:22 +0300

    add navHost

--- composeApp/src/commonMain/kotlin/org/aasoft/easypos/controller/SellController.kt
+++ composeApp/src/commonMain/kotlin/org/aasoft/easypos/controller/SellController.kt
@@ -4,11 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import org.aasoft.easypos.data.ItemsViewModel
 import org.aasoft.easypos.data.SellItem
 import org.aasoft.easypos.ui.SellScreen
@@ -32,6 +29,7 @@ fun SellController(modifier: Modifier= Modifier, model: ItemsViewModel )
             {
                val i = sellItems.indexOf(item)
                 sellItems[i]=sellItems[i].copy(quantity = sellItems[i].quantity+1)
+                model.setItem(i,sellItems[i])
             }
             else
             {
